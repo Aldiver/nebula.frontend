6ccdae8859eec817fa9b897eb73a3f5bcaeba166
Aldiver <frontend@mountainstream.co>
2023-08-30 17:15:54 +0800

    not ready for the real world

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,11 +1,15 @@
-import { useForm } from "react-hook-form";
+import React, { useEffect, useState } from "react";
 import dictLogo from "../assets/dict-logo-hz.png";
-import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
-const LoginScreen = () => {
-  const { register, handleSubmit } = useForm();
+interface LoginForm {
+  email: string;
+  password: string;
+}
 
+const LoginScreen: React.FC = () => {
   const [theme, setTheme] = useState("light");
+  const { register, handleSubmit, setValue } = useForm<LoginForm>();
 
   useEffect(() => {
     const htmlElement = document.querySelector("html");
@@ -15,8 +19,25 @@ const LoginScreen = () => {
     }
   }, [theme]);
 
-  const submitForm = (data: any) => {
-    console.log(data);
+  const submitForm = async (data: LoginForm) => {
+    try {
+      const response = await fetch("http://192.168.10.92:8000/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        const responseData = await response.json();
+        console.log(responseData);
+      } else {
+        console.error("Login failed");
+      }
+    } catch (error) {
+      console.error("An error occurred", error);
+    }
   };
 
   return (
@@ -24,8 +45,8 @@ const LoginScreen = () => {
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <div className="text-center lg:text-left ">
-              <img src={dictLogo} alt="DICT-LOGO" className="pt-6 " />
+            <div className="text-center lg:text-left">
+              <img src={dictLogo} alt="DICT-LOGO" className="pt-6" />
             </div>
 
             <div className="card-body pt-2">
@@ -35,6 +56,7 @@ const LoginScreen = () => {
                 </label>
                 <input
                   type="text"
+                  {...register("email")}
                   placeholder="email"
                   className="input input-bordered"
                 />
@@ -45,6 +67,7 @@ const LoginScreen = () => {
                 </label>
                 <input
                   type="password"
+                  {...register("password")}
                   placeholder="password"
                   className="input input-bordered"
                 />
@@ -55,7 +78,9 @@ const LoginScreen = () => {
                 </label>
               </div>
               <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
+                <button type="submit" className="btn btn-primary">
+                  Login
+                </button>
               </div>
             </div>
           </div>
